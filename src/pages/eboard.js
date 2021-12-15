4ec58f34ba821ad5bc54c8281729247b14b431b2
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-12-15 12:33:19 -0800

    added updated SWIFT eboard with temporary placeholder images

--- src/pages/eboard.js
+++ src/pages/eboard.js
@@ -6,68 +6,83 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {fab, faTwitterSquare, faFacebook, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
 
 
+/* First Row */ 
 function One() {
     return (
      <section className={styles.section}>
       <div class="row core-values">
-        <div class="col col--6 centered">   
-          <img src={useBaseUrl('img/eboard-images/Taylor_Nguyen.jpg')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Taylor Nguyen</p>
-          <p className={styles.ePosition}>President</p>
+        {/* Lawrence Kim */}
+        <div class="col col--4 centered">   
+          <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Lawrence Kim</p>
+          <p className={styles.ePosition}>Technical Assistant</p>
           <p> 
-            <a href="https://www.linkedin.com/in/silass1/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
-       
         </div>
-        <div class="col col--6 centered">
-            <img src={useBaseUrl('img/eboard-images/Louie_Hernandez.jpg')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Louie Hernandez</p>
-          <p className={styles.ePosition}>Co-President</p>
+        {/* Angela Lee */}
+        <div class="col col--4 centered">   
+          <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Angela Lee</p>
+          <p className={styles.ePosition}>Technical Assistant</p>
+          <p> 
+            <a href="#"> 
+                <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+            </a>
+          </p>
+        {/*Dylan Wong*/}
+        </div>
+        <div class="col col--4 centered">
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Dylan Wong</p>
+          <p className={styles.ePosition}>Technical Assistant</p>
           <p>
-            <a href="https://www.linkedin.com/in/louieh326/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
-    
       </div>
     </section>
     )
 }
 
+// Second Row
 function Two() {
     return (
         <section>
       <div class="row core-values">
+        {/* Justin Covairt */}
         <div class="col col--4 centered">   
-          <img src={useBaseUrl('img/eboard-images/Taylor_Nguyen.jpg')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Taylor Nguyen</p>
-          <p className={styles.ePosition}>VP of Operations</p>
+          <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Justin Covairt</p>
+          <p className={styles.ePosition}>Technical Assistant</p>
           <p>
-            <a href="https://www.linkedin.com/in/taylorsnguyen/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
-        
+        {/* Alejandro Rivera*/}
         <div class="col col--4 centered">
-            <img src={useBaseUrl('img/eboard-images/Hannah_Kim.png')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Hannah Kim</p>
-          <p className={styles.ePosition}>Treasurer</p>
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Alejandro Rivera</p>
+          <p className={styles.ePosition}>Director - Logistics</p>
           <p>
-            <a href="https://www.linkedin.com/in/hannaheunkyungkim/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
+      {/* Luis Velasquez */}
         <div class="col col--4 centered">
-        <img src={useBaseUrl('img/eboard-images/Angela_Lee.jpg')} className={styles.socialImg}/>
-          <p className={styles.socialNames}>Angela Lee</p>
-          <p className={styles.ePosition}>VP of Administrative Affairs</p>
+        <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg}/>
+          <p className={styles.socialNames}>Luis Velasquez</p>
+          <p className={styles.ePosition}>Technical Assistant</p>
           <p>
-            <a href="https://www.linkedin.com/in/angelalee240"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
@@ -78,56 +93,219 @@ function Two() {
     )
 }
 
+// Third row
 function Three() {
     return (
         <section >
       <div class="row core-values">
-        <div class="col col--3 centered">   
-          <img src={useBaseUrl('img/eboard-images/Somar_Dakak.jpg')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Somar Dakak</p>
+        {/* Robinson Tran */}
+        <div class="col col--4 centered">   
+          <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Robinson Tran</p>
           <p className={styles.ePosition}>Director of SWIFT Labs</p>
           <p>
-            <a href="https://www.linkedin.com/in/somar-dakak/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
-        <div class="col col--3 centered">
-        <img src={useBaseUrl('img/eboard-images/Brice_Lauer.png')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Brice Lauer</p>
-          <p className={styles.ePosition}>Director of SWIFT Academy</p>
+        {/* Alexey Tselevich */}
+        <div class="col col--4 centered">
+        <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Alexey Tselevich</p>
+          <p className={styles.ePosition}>VP of Operations</p>
           <p>
-            <a href="https://www.linkedin.com/in/blauersec/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
-        <div class="col col--3 centered">
-            <img src={useBaseUrl('img/eboard-images/Jessica_Foronda.jpg')} className={styles.socialImg} />
-          <p className={styles.socialNames}>Jessica Foronda</p>
-          <p className={styles.ePosition}>Director of Marketing</p>
+        {/* Gabriel Fok */}
+        <div class="col col--4 centered">
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+          <p className={styles.socialNames}>Gabriel Fok</p>
+          <p className={styles.ePosition}>Director - SWIFT Academy</p>
           <p>
-            <a href="https://www.linkedin.com/in/jessicaforonda/"> 
+            <a href="#"> 
                 <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
             </a>
           </p>
         </div>
-        <div class="col col--3 centered">
-        <img src={useBaseUrl('img/eboard-images/Jennifer_Schwartz.jpg')}  className={styles.socialImg}/>
-          <p className={styles.socialNames}>Jennifer Schwartz</p>
-          <p className={styles.ePosition}>Head Chair of Conference</p>
-          <p>
-            <a href="https://www.linkedin.com/in/jenni-schwartz/"> 
-                <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
-            </a>
-          </p>
-        </div>
-
       </div>
     </section>
     )
 }
 
+// Fourth row
+function Four() {
+  return (
+        <section >
+          <div class="row core-values">
+            {/* Taylor Nguyen */}
+            <div class="col col--12 centered">   
+              <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Taylor Nguyen</p>
+              <p className={styles.ePosition}>PRESIDENT</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+          </div>
+    </section>
+    )
+}
+
+// Fifth row
+function Five() {
+ return (
+        <section >
+          <div class="row core-values">
+            {/* Mercedes Wendy Gonzales  */}
+            <div class="col col--4 centered">   
+              <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Mercedes Wendy Gonzales</p>
+              <p className={styles.ePosition}>Director - Marketing</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Jessica Foronda */}
+            <div class="col col--4 centered">
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Jessica Foronda</p>
+              <p className={styles.ePosition}>VP of Administration</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Jasmine Weddle */}
+            <div class="col col--4 centered">
+                <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Jasmine Weddle</p>
+              <p className={styles.ePosition}>Director - Outreach</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+          </div>
+    </section>
+    )
+}
+
+// Sixth row
+function Six() {
+ return (
+        <section >
+          <div class="row core-values">
+            {/* Aaron Diaz */}
+            <div class="col col--4 centered">   
+              <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Aaron Diaz</p>
+              <p className={styles.ePosition}>Administrative Assistant</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Felipe Aceves */}
+            <div class="col col--4 centered">
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Felipe Aceves</p>
+              <p className={styles.ePosition}>Treasurer</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Jessica Leung */}
+            <div class="col col--4 centered">
+                <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Jessica Leung</p>
+              <p className={styles.ePosition}>Conference Chair</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+          </div>
+    </section>
+    )
+}
+
+// Seventh row
+function Seven() {
+ return (
+        <section >
+          <div class="row core-values">
+            {/* Emily Han */}
+            <div class="col col--4 centered">   
+              <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Emily Han</p>
+              <p className={styles.ePosition}>Social Media</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Thaliea Ramsey */}
+            <div class="col col--4 centered">
+            <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Thaliea Ramsey</p>
+              <p className={styles.ePosition}>Administrative Assistant</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+            {/* Maymoonah Mohammed */}
+            <div class="col col--4 centered">
+                <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} />
+              <p className={styles.socialNames}>Maymoonah Mohammed</p>
+              <p className={styles.ePosition}>Web Designer</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+          </div>
+    </section>
+    )
+}
+
+// Eighth row
+function Eight() {
+return (
+        <section >
+          <div class="row core-values">
+            {/* Melvin Karuga */}
+            <div class="col col-- 4 centered">   
+              <img src={useBaseUrl('img/user_placeholder.jpg')} className={styles.socialImg} style={{height: "240px"}} />
+              <p className={styles.socialNames}>Melvin Karuga</p>
+              <p className={styles.ePosition}>Graphic Designer</p>
+              <p>
+                <a href="#"> 
+                    <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcons}/> 
+                </a>
+              </p>
+            </div>
+          </div>
+    </section>
+    )
+}
 
 function Eboard() {
     return (
@@ -151,6 +329,11 @@ function Eboard() {
             <One />
             <Two />
             <Three />
+            <Four />
+            <Five />
+            <Six />
+            <Seven />
+            <Eight />
           </section>
         </div>
 
