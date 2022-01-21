35a28da1c9fc59aead4446d7661608d4608be193
Jacob Jayme <jaymerjacob@gmail.com>
2022-01-21 10:52:43 -0800

    Deploy website - based on 472a3d873235012751861ed6892b3d52711a09b6

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\internet\\swift-website-2021\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\internet\\swift-website-2021\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\internet\\swift-website-2021\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\internet\\swift-website-2021\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\internet\\swift-website-2021\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\internet\\swift-website-2021\\src\\css\\custom.css"),
-];
